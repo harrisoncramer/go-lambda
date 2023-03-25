b9ed940e96ad94b048ab79655569dbdf7f73814e
Harrison Cramer <harrisoncramer@gmail.com>
2023-03-24 23:26:42 -0400

    Renaming variables, adding documentation

--- api_gateway.tf
+++ api_gateway.tf
@@ -2,32 +2,35 @@
 # See this tutorial: https://registry.terraform.io/providers/hashicorp/aws/2.34.0/docs/guides/serverless-with-aws-lambda-and-api-gateway
 
 # Create an API Gateway REST API
-resource "aws_api_gateway_rest_api" "api" {
-  name = "hello_world_api"
+resource "aws_api_gateway_rest_api" "hello_world_api" {
+  name        = "hello_world_api"
+  description = "The API gateway for our hello world application. We could add more functions in the future if we wanted to."
 }
 
-# Create a resource within the REST API
-resource "aws_api_gateway_resource" "resource" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+# Create a resource within the REST API that proxies requests to resources
+# The special path_part value "{proxy+}" activates proxy behavior, which means that this resource will match any request path.
+# This means that all requests (GET/POST etc) on ALL paths will go through this proxy
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.hello_world_api.id
+  parent_id   = aws_api_gateway_rest_api.hello_world_api.root_resource_id
   path_part   = "{proxy+}"
 }
 
-# Create a method for the resource (in this case, a GET resource)
 resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "GET"
+  rest_api_id   = aws_api_gateway_rest_api.hello_world_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
   authorization = "NONE"
 }
 
-# Next create an Integration resource to integrate the above method with a backend endpoint, also known as the integration endpoint, by forwarding the incoming request to a specified integration endpoint URI.
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource.id
+# Next create an Integration resource to integrate the proxy with our backend endpoint, our lambda.
+# Each method on an API gateway resource has an integration which specifies where incoming requests are routed. Add the following configuration to specify that requests to this method should be sent to the Lambda function.
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.hello_world_api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
   http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
+  type                    = "AWS_PROXY" # Call the lambda
   uri                     = aws_lambda_function.hello_world.invoke_arn
 }
 
@@ -37,12 +40,12 @@ resource "aws_lambda_permission" "api_permission" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.hello_world.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+  source_arn    = "${aws_api_gateway_rest_api.hello_world_api.execution_arn}/*/*"
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
-  depends_on  = [aws_api_gateway_integration.integration] # Wait for the API gateway integration to create
-  rest_api_id = aws_api_gateway_rest_api.api.id
+  depends_on  = [aws_api_gateway_integration.lambda_integration] # Wait for the API gateway integration to create
+  rest_api_id = aws_api_gateway_rest_api.hello_world_api.id
   stage_name  = "test"
 }
 
